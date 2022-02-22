1488de8c7e3a0701f60b6a6b395787d0ca7cfc05
Angel Martinez <angelmartinez2780@gmail.com>
2022-02-22 17:09:46 -0600

    Template con firebase

---
+++ src/app/interfaces/info-equipo.interface.ts
@@ -0,0 +1,9 @@
+
+export interface infoEquipo{
+    frase?:string;
+    nombre?:string;
+    subtitulo?:string;
+    twitter?:string;
+    url?:string;
+   
+}
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { infoEquipo } from '../interfaces/info-equipo.interface';
 import { infoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
@@ -9,10 +10,16 @@ export class InfoPaginaService {
 
   //definir interfaz
   info:infoPagina={};
+  equipo:infoEquipo={};
   cargada= false;
   constructor(private http:HttpClient) { 
     //console.log('servicio de infopagina cargado');
+    this.cargarInfo();
+    this.cargarEquipo();
+    
+  }
 
+  private cargarInfo(){
     //leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
     .subscribe((resp:infoPagina) =>{
@@ -21,6 +28,18 @@ export class InfoPaginaService {
       this.cargada = true;
       this.info = resp;
       console.log(this.info);
+    });
+  } 
+
+  private cargarEquipo(){
+    //leer el archivo JSON
+    this.http.get('https://angular-html-3fc44-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe((resp:infoEquipo) =>{
+
+      //almacenar datos
+      this.cargada = true;
+      this.equipo = resp;
+      console.log(resp);
     });
   }
 }
